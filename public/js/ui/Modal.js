87cd30a77d92b245863781c0eabef5a51e36f996
Elias <kel20@inbox.ru>
2023-08-13 20:14:41 +0300

    first commit

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -11,8 +11,13 @@ class Modal {
    * Если переданный элемент не существует,
    * необходимо выкинуть ошибку.
    * */
-  constructor(element){
+  constructor(element) {
+    if (!element) {
+      throw new Error('Элемент не существует');
+    }
 
+    this.element = element;
+    this.registerEvents();
   }
 
   /**
@@ -21,7 +26,11 @@ class Modal {
    * (с помощью метода Modal.onClose)
    * */
   registerEvents() {
+    const modalCloseButtons = this.element.querySelectorAll('[data-dismiss="modal"]');
 
+    modalCloseButtons.forEach(element => {
+      element.addEventListener('click', (e) => this.onClose(e));
+    });
   }
 
   /**
@@ -29,19 +38,19 @@ class Modal {
    * Закрывает текущее окно (Modal.close())
    * */
   onClose(e) {
-
+    this.close();
   }
   /**
    * Открывает окно: устанавливает CSS-свойство display
    * со значением «block»
    * */
   open() {
-
+    this.element.style.display = 'block';
   }
   /**
    * Закрывает окно: удаляет CSS-свойство display
    * */
-  close(){
-
+  close() {
+    this.element.style.display = '';
   }
 }
